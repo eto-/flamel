45299ef0ecef2233f694742e66cceb66e0904212
Alessandro Razeto <razeto@lngs.infn.it>
2015-06-04 19:53:53 +0200

    Handle sigint

--- main.cc
+++ main.cc
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <stdexcept>
+#include <csignal>
 #include "sibilla.hh"
 #include "giotto.hh"
 #include "flamel.hh"
 #include "omero.hh"
 
+bool quit = false;
 int main (int argc, char* argv[]) {
   sibilla::evoke ().parse (argc, argv);
 
@@ -16,8 +18,20 @@ int main (int argc, char* argv[]) {
 
   f.start ();
 
+  signal(SIGINT, [](int signum) { std::cerr << "signal caught, clean exiting" << std::endl; quit = true; });
+  sigset_t mask, orig_mask;
+  sigemptyset (&mask);
+  sigaddset (&mask, SIGINT);
+
   for (int n = sibilla::evoke ()["events"].as<int>(); n > 0;) {
+    if (quit) break;
+    
+    sigprocmask(SIG_BLOCK, &mask, &orig_mask);
+
     auto v = f.loop ();
+
+    sigprocmask(SIG_SETMASK, &orig_mask, NULL);
+
     n -= v.size ();
     for (int i = 0; i < v.size (); i++) {
       std::unique_ptr<evaristo> e = std::move(v[i]);
