277c4256e60c2fbdb4d8d93641a89067f48d745a
Alessandro Razeto <razeto@lngs.infn.it>
2017-06-15 22:02:19 +0200

    Ignore DES error on not compatible boards

--- flamel.cc
+++ flamel.cc
@@ -125,7 +125,7 @@ void flamel::init_link () {
 
 void flamel::init_channels () {
   CAEN_DGTZ_ErrorCode err = CAEN_DGTZ_SetDESMode (handle_, sibilla::evoke ()("des-mode") ?  CAEN_DGTZ_ENABLE : CAEN_DGTZ_DISABLE);
-  if (err != CAEN_DGTZ_Success) ATTILA << " CAEN_DGTZ_SetDESMode(" << handle_ << ",true): " << caen_error (err);
+  if (err != CAEN_DGTZ_Success && (err != CAEN_DGTZ_FunctionNotAllowed || sibilla::evoke ()("des-mode"))) ATTILA << " CAEN_DGTZ_SetDESMode(" << handle_ << ",true): " << caen_error (err);
 
   set_register (sibilla::evoke ()("test-pattern") ? 0x8004 : 0x8008, 1 << 3);
   set_register (sibilla::evoke ()("overlap-trigger") ? 0x8004 : 0x8008, 1 << 1);
