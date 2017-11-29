8a00bfdb39b1c9adaf7e34ae0a9ea87191270858
Alessandro Razeto <razeto@lngs.infn.it>
2017-11-29 09:27:42 +0100

    Use new types

--- evaristo.hh
+++ evaristo.hh
@@ -1,11 +1,12 @@
 #ifndef EVARISTO_HH
 #define EVARISTO_HH
 #include <ostream>
+#include <memory>
 
 struct evaristo {
-  u_int16_t marker, header_length;
-  u_int32_t counter, time_tag, n_samples, cpu_time_ms, n_channels, unused[4];
-  u_int16_t samples[0];
+  uint16_t marker, header_length;
+  uint32_t counter, time_tag, n_samples, cpu_time_ms, n_channels, unused[4];
+  uint16_t samples[0];
 };
 
 std::ostream & operator << (std::ostream&, const evaristo&);
