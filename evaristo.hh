9c0335fe204900f9ed281511fb5f0f52ff5272d3
Alessandro Razeto <razeto@lngs.infn.it>
2018-12-17 14:09:37 +0100

    experimental code

--- evaristo.hh
+++ evaristo.hh
@@ -3,13 +3,25 @@
 #include <ostream>
 #include <memory>
 
+#define EVARISTO_VERSION 200 // major * 100 + minor
+
 struct evaristo {
-  uint16_t marker, header_length;
-  uint32_t counter, time_tag, n_samples, cpu_time_ms, n_channels, unused[4];
-  uint16_t samples[0];
+  evaristo() {
+    marker = 0xFFFF;
+    header_length = sizeof(evaristo) / sizeof(uint16_t);
+    version = EVARISTO_VERSION;
+  }
+  uint16_t marker, header_length;  // length in samples
+  uint32_t counter, time_tag, n_samples, cpu_time_ms; 
+  uint16_t n_channels, version, unused[8];
+  struct channel_data {
+    uint16_t channel, n_samples, unused[2];
+    uint16_t samples[0];
+  } data[0];
 };
 
 std::ostream & operator << (std::ostream&, const evaristo&);
+
 
 struct metadata {
   int board;
