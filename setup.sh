9ded5c6b6e17d787a0b7c11e433857f219906bf5
Alessandro Razeto <razeto@lngs.infn.it>
2019-09-15 08:55:37 +0200

    Do not link with specific caen libraries

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 if [ -z `which ups` ]; then
-  export CAENDIGITIZER_INC=/usr/local/caen/CAENDigitizer_2.9.1/include
-  export CAENDIGITIZER_LIB=/usr/local/caen/CAENDigitizer_2.9.1/lib/x86_64
-  export CAENCOMM_LIB=/usr/local/caen/CAENComm-1.2/lib/x64
-  export CAENVME_LIB=/usr/local/caen/CAENVMELib-2.41/lib/x64
+  export CAENDIGITIZER_INC=/usr/local/caen/CAENDigitizer/include
+  export CAENDIGITIZER_LIB=/usr/local/caen/CAENDigitizer/lib/x86_64
+  export CAENCOMM_LIB=/usr/local/caen/CAENComm/lib/x64
+  export CAENVME_LIB=/usr/local/caen/CAENVMELib/lib/x64
   export BOOST_INC=/usr/include
   export BOOST_LIB=/usr/lib/x86_64-linux-gnu
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CAENDIGITIZER_LIB:$CAENCOMM_LIB:$CAENVME_LIB
