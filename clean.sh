298f806d1442610ae794aeaa4b97071e379a608f
Alessandro Razeto <razeto@lngs.infn.it>
2021-10-15 12:18:49 +0200

    Added initial effort

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+rm config.status configure configure~ config.log
+rm Makefile
+rm -rf autom4te.cache depcomp
+rm aclocal.m4
+rm compile install-sh missing Makefile.in
+rm config.guess config.sub
+rm config.h.in config.h
+rm stamp-h1
+
