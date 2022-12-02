0db4e44c004250e750a7027e99693e005e03d8d3
Alessandro Razeto <razeto@lngs.infn.it>
2022-12-02 15:50:03 +0100

    Added external caen setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
-export CAENDIGITIZER_INC=/usr/local/caen/CAENDigitizer/include
-export CAENDIGITIZER_LIB=/usr/local/caen/CAENDigitizer/lib/x86_64
-export CAENCOMM_LIB=/usr/local/caen/CAENComm/lib/x64
-export CAENVME_LIB=/usr/local/caen/CAENVMELib/lib/x64
+source /usr/local/caen/setup.sh
 export BOOST_INC=/usr/include
 export BOOST_LIB=/usr/lib/x86_64-linux-gnu
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CAENDIGITIZER_LIB:$CAENCOMM_LIB:$CAENVME_LIB
 choose_root current
